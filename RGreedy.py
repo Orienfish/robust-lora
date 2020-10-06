f1b2a9ad750027db8d6090b9ad740f3b68f9d7e8
Orienfish <x1yu@ucsd.edu>
2020-10-06 15:32:59 -0700

    add pdr and lifetime to benefit

--- RGreedy.py
+++ RGreedy.py
@@ -10,7 +10,6 @@ import propagation
 ########################################
 # RGreedy Alg
 ########################################
-penalty = 0 # Penalize redundant connectivity
 
 def GetDist(propFunc, params):
 	'''
@@ -209,6 +208,8 @@ def DeviceConfiguration(sr_info_cur, sr_info, G, PL, N_kq_cur, m_gateway_cur, \
 		sr_info_cur: updated device configuration
 		N_kq_cur: updated collision dictionary
 		m_gateway_cur: updated m-gateway connectivity status
+		PDR: packet delivery ratio due to the configuraton
+		Lifetime: lifetime of the device due to the configuration
 	'''
 	SF_cnt = len(params.SF)
 	CH_cnt = len(params.CH)
@@ -217,12 +218,10 @@ def DeviceConfiguration(sr_info_cur, sr_info, G, PL, N_kq_cur, m_gateway_cur, \
 	for k in range(SF_min, SF_cnt):
 
 		sr_info_cur[sr_idx, 2] = k
-
 		for q in range(CH_cnt):
 
 			sr_info_cur[sr_idx, 4] = q
 			N_kq_cur[str(k) + '_' + str(q)].append(sr_idx)
-
 			for Ptx in params.Ptx:
 				
 				sr_info_cur[sr_idx, 3] = Ptx
@@ -282,7 +281,7 @@ def DeviceConfiguration(sr_info_cur, sr_info, G, PL, N_kq_cur, m_gateway_cur, \
 		sr_info_cur[sr_idx, :] = np.copy(sr_info[sr_idx, :])
 
 
-	return succ
+	return succ, PDR, Lifetime
 
 
 def RGreedyAlg(sr_info_ogn, G_ogn, PL, dist, params):
@@ -324,6 +323,8 @@ def RGreedyAlg(sr_info_ogn, G_ogn, PL, dist, params):
 
 	m_gateway = np.zeros((sr_cnt, 1)) # Current gateway connectivity at each end node
 	uncover_old = sr_cnt * params.M
+	PDR_old = np.zeros((sr_cnt, 1))
+	Lifetime_old = np.zeros((sr_cnt, 1))
 	rounds = 0
 
 	# Start greedily place gateway
@@ -342,14 +343,18 @@ def RGreedyAlg(sr_info_ogn, G_ogn, PL, dist, params):
 			sr_info_cur = np.copy(sr_info)
 			N_kq_cur = copy.deepcopy(N_kq)
 			m_gateway_cur = np.copy(m_gateway)
+			PDR_cur = np.copy(PDR_old)
+			Lifetime_cur = np.copy(Lifetime_old)
 
 			dist_gw_idx = dist[:, gw_idx] # distance to gateway idx
 			sort_idx = np.argsort(dist_gw_idx, axis=0) # index of distance sorting
 			for i in range(sr_cnt):
 				sr_idx = int(sort_idx[i])
+
 				if dist[sr_idx, gw_idx] > maxDist[len(params.SF)-1]:
 					# the following sensors exceed the maximum communication range
 					# therefore we do not need to consider them
+					logging.info('Exceeds maximum communication range!')
 					break
 
 				# Calculate the min SF that can used by this end device according to distance
@@ -358,18 +363,27 @@ def RGreedyAlg(sr_info_ogn, G_ogn, PL, dist, params):
 				logging.debug('SF_min: {}'.format(SF_min))
 				# Try to assign the minimum SF and channel based on the current sr_info
 				# and collision-possible nodes N_kq
-				succ = DeviceConfiguration(sr_info_cur, \
+				succ, PDR_i, Lifetime_i = DeviceConfiguration(sr_info_cur, \
 					sr_info, G, PL, N_kq_cur, m_gateway_cur, params, sr_idx, gw_idx, SF_min)
+				logging.debug('succ: {} PDR_i: {} Lifetime_i: {}'.format(succ, PDR_i, Lifetime_i))
 				
 				if not succ:
 					# All assignment attempts are failed, no more assignment can be made
 					break
+
+				# Update the new PDR and lifetime after assignment
+				PDR_cur[sr_idx] = PDR_i
+				Lifetime_cur[sr_idx] = Lifetime_i
 						
 			# Calculate benefit of placing gateway at this location
 			conn_cur = np.ones((sr_cnt, 1)) * params.M - m_gateway_cur
 			uncover_new = np.sum(conn_cur[conn_cur > 0])
-			# Benefit function: promote new connectivity and penalize redundant connectivity
-			bnft = uncover_old - uncover_new
+			# Benefit function: promote new connectivity and PDR, lifetime constraints
+			bnft_cov = uncover_old - uncover_new
+			bnft_pdr = params.w_pdr * np.sum(PDR_cur - np.ones((sr_cnt, 1)) * params.PDR_th)
+			bnft_lifetime = params.w_lifetime * np.sum(Lifetime_cur - np.ones((sr_cnt, 1)) * params.Lifetime_th)
+			bnft = bnft_cov + bnft_pdr + bnft_lifetime
+			logging.debug('cov: {} pdr: {} lifetime: {}'.format(bnft_cov, bnft_pdr, bnft_lifetime))
 
 			# Update global best benefit value if necessary
 			if bnft > bnft_best:
@@ -379,6 +393,8 @@ def RGreedyAlg(sr_info_ogn, G_ogn, PL, dist, params):
 				best_N_kq = N_kq_cur
 				best_m_gateway = m_gateway_cur
 				best_uncover = uncover_new
+				best_PDR = PDR_cur
+				best_Lifetime = Lifetime_cur
 
 			# Logging
 			logging.debug('gw_idx: {} Benefit: {} Max bnft: {} Max idx: {}'.format(\
@@ -388,8 +404,8 @@ def RGreedyAlg(sr_info_ogn, G_ogn, PL, dist, params):
 			G[gw_idx, 2] = 0
 
 			# Early ending if already satisfy all uncovers
-			if uncover_new == 0:
-				break
+			#if uncover_new == 0:
+			#	break
 
 		# Check if there is no benefit to gain, end the searching while loop
 		if bnft_best == 0:
@@ -402,6 +418,8 @@ def RGreedyAlg(sr_info_ogn, G_ogn, PL, dist, params):
 		N_kq = best_N_kq
 		m_gateway = best_m_gateway
 		uncover_old = best_uncover
+		PDR_old = best_PDR
+		Lifetime_old = best_Lifetime
 
 		logging.info('Placed gateway #{} at grid {} [{},{}]'.format( \
 			rounds, best_idx, G[best_idx, 0], G[best_idx, 1]))
