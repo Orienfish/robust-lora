560945514f3434ea51587b7a2069d094b0cfc922
Orienfish <x1yu@ucsd.edu>
2020-10-19 19:49:22 -0700

    ready to rerun

--- run.sh
+++ run.sh
@@ -1,121 +1,121 @@
 #!/bin/bash
 
-#cp ~/Github/robust-lora/res/sr_RGreedyc_1_264_0.txt ./sr_loc.txt
-#cp ~/Github/robust-lora/res/gw_RGreedyc_1_264_0.txt ./gw_loc.txt
-#destlog=log1
-#start=`date +%s`
-#./waf --run "adr --MType=Confirmed" > "$destlog" 2>&1
-#end=`date +%s`
-#runtime=$((end-start))
-#echo "$runtime" >> "$destlog"
-#mv ./nodeData.txt ./res/nodeData1.txt
-#mv ./nodeEE.txt ./res/nodeEE1.txt
-#mv ./globalPerformance.txt ./res/globalPerformance1.txt
-#mv ./phyPerformance.txt ./res/phyPerformance1.txt
-
-#cp ~/Github/robust-lora/res/sr_RGreedy_2_264_0.txt ./sr_loc.txt
-#cp ~/Github/robust-lora/res/gw_RGreedy_2_264_0.txt ./gw_loc.txt
-#destlog=log2
-#start=`date +%s`
-#./waf --run "adr --MType=Confirmed" > "$destlog" 2>&1
-#end=`date +%s`
-#runtime=$((end-start))
-#echo "$runtime" >> "$destlog"
-#mv ./nodeData.txt ./res/nodeData2.txt
-#mv ./nodeEE.txt ./res/nodeEE2.txt
-#mv ./globalPerformance.txt ./res/globalPerformance2.txt
-#mv ./phyPerformance.txt ./res/phyPerformance2.txt
-
-#cp ~/Github/robust-lora/res/sr_RGreedy_3_264_0.txt ./sr_loc.txt
-#cp ~/Github/robust-lora/res/gw_RGreedy_3_264_0.txt ./gw_loc.txt
-#destlog=log3
-#start=`date +%s`
-#./waf --run "adr --MType=Confirmed" > "$destlog" 2>&1
-#end=`date +%s`
+cp ~/Github/robust-lora/res/sr_RGreedyc_1_264_0.txt ./sr_loc.txt
+cp ~/Github/robust-lora/res/gw_RGreedyc_1_264_0.txt ./gw_loc.txt
+destlog=log1
+start=`date +%s`
+./waf --run "adr --MType=Confirmed" > "$destlog" 2>&1
+end=`date +%s`
 runtime=$((end-start))
-#echo "$runtime" >> "$destlog"
-#mv ./nodeData.txt ./res/nodeData3.txt
-#mv ./nodeEE.txt ./res/nodeEE3.txt
-#mv ./globalPerformance.txt ./res/globalPerformance3.txt
-#mv ./phyPerformance.txt ./res/phyPerformance3.txt
+echo "$runtime" >> "$destlog"
+mv ./nodeData.txt ./res/nodeData1.txt
+mv ./nodeEE.txt ./res/nodeEE1.txt
+mv ./globalPerformance.txt ./res/globalPerformance1.txt
+mv ./phyPerformance.txt ./res/phyPerformance1.txt
 
-#cp ~/Github/robust-lora/res/sr_RGreedyc_1_264_0.txt ./sr_loc.txt
-#cp ~/Github/robust-lora/res/gw_RGreedyc_1_264_0.txt ./gw_loc.txt
-#destlog=log11
-#start=`date +%s`
-#./waf --run "adr --MType=Confirmed --nDownGateways=1" > "$destlog" 2>&1
-#end=`date +%s`
-#runtime=$((end-start))
-#echo "$runtime" >> "$destlog"
-#mv ./nodeData.txt ./res/nodeData11.txt
-#mv ./nodeEE.txt ./res/nodeEE11.txt
-#mv ./globalPerformance.txt ./res/globalPerformance11.txt
-#mv ./phyPerformance.txt ./res/phyPerformance11.txt
+cp ~/Github/robust-lora/res/sr_RGreedy_2_264_0.txt ./sr_loc.txt
+cp ~/Github/robust-lora/res/gw_RGreedy_2_264_0.txt ./gw_loc.txt
+destlog=log2
+start=`date +%s`
+./waf --run "adr --MType=Confirmed" > "$destlog" 2>&1
+end=`date +%s`
+runtime=$((end-start))
+echo "$runtime" >> "$destlog"
+mv ./nodeData.txt ./res/nodeData2.txt
+mv ./nodeEE.txt ./res/nodeEE2.txt
+mv ./globalPerformance.txt ./res/globalPerformance2.txt
+mv ./phyPerformance.txt ./res/phyPerformance2.txt
 
-#cp ~/Github/robust-lora/res/sr_RGreedy_2_264_0.txt ./sr_loc.txt
-#cp ~/Github/robust-lora/res/gw_RGreedy_2_264_0.txt ./gw_loc.txt
-#destlog=log21
-#start=`date +%s`
-#./waf --run "adr --MType=Confirmed --nDownGateways=1" > "$destlog" 2>&1
-#end=`date +%s`
-#runtime=$((end-start))
-#echo "$runtime" >> "$destlog"
-#mv ./nodeData.txt ./res/nodeData21.txt
-#mv ./nodeEE.txt ./res/nodeEE21.txt
-#mv ./globalPerformance.txt ./res/globalPerformance21.txt
-#mv ./phyPerformance.txt ./res/phyPerformance21.txt
+cp ~/Github/robust-lora/res/sr_RGreedy_3_264_0.txt ./sr_loc.txt
+cp ~/Github/robust-lora/res/gw_RGreedy_3_264_0.txt ./gw_loc.txt
+destlog=log3
+start=`date +%s`
+./waf --run "adr --MType=Confirmed" > "$destlog" 2>&1
+end=`date +%s`
+runtime=$((end-start))
+echo "$runtime" >> "$destlog"
+mv ./nodeData.txt ./res/nodeData3.txt
+mv ./nodeEE.txt ./res/nodeEE3.txt
+mv ./globalPerformance.txt ./res/globalPerformance3.txt
+mv ./phyPerformance.txt ./res/phyPerformance3.txt
 
-#cp ~/Github/robust-lora/res/sr_RGreedy_3_264_0.txt ./sr_loc.txt
-#cp ~/Github/robust-lora/res/gw_RGreedy_3_264_0.txt ./gw_loc.txt
-#destlog=log31
-#start=`date +%s`
-#./waf --run "adr --MType=Confirmed --nDownGateways=1" > "$destlog" 2>&1
-#end=`date +%s`
-#runtime=$((end-start))
-#echo "$runtime" >> "$destlog"
-#mv ./nodeData.txt ./res/nodeData31.txt
-#mv ./nodeEE.txt ./res/nodeEE31.txt
-#mv ./globalPerformance.txt ./res/globalPerformance31.txt
-#mv ./phyPerformance.txt ./res/phyPerformance31.txt
+cp ~/Github/robust-lora/res/sr_RGreedyc_1_264_0.txt ./sr_loc.txt
+cp ~/Github/robust-lora/res/gw_RGreedyc_1_264_0.txt ./gw_loc.txt
+destlog=log11
+start=`date +%s`
+./waf --run "adr --MType=Confirmed --nDownGateways=1" > "$destlog" 2>&1
+end=`date +%s`
+runtime=$((end-start))
+echo "$runtime" >> "$destlog"
+mv ./nodeData.txt ./res/nodeData11.txt
+mv ./nodeEE.txt ./res/nodeEE11.txt
+mv ./globalPerformance.txt ./res/globalPerformance11.txt
+mv ./phyPerformance.txt ./res/phyPerformance11.txt
 
-#cp ~/Github/robust-lora/res/sr_RGreedyc_1_264_0.txt ./sr_loc.txt
-#cp ~/Github/robust-lora/res/gw_RGreedyc_1_264_0.txt ./gw_loc.txt
-#destlog=log12
-#start=`date +%s`
-#./waf --run "adr --MType=Confirmed --nDownGateways=2" > "$destlog" 2>&1
-#end=`date +%s`
-#runtime=$((end-start))
-#echo "$runtime" >> "$destlog"
-#mv ./nodeData.txt ./res/nodeData12.txt
-#mv ./nodeEE.txt ./res/nodeEE12.txt
-#mv ./globalPerformance.txt ./res/globalPerformance12.txt
-#mv ./phyPerformance.txt ./res/phyPerformance12.txt
+cp ~/Github/robust-lora/res/sr_RGreedy_2_264_0.txt ./sr_loc.txt
+cp ~/Github/robust-lora/res/gw_RGreedy_2_264_0.txt ./gw_loc.txt
+destlog=log21
+start=`date +%s`
+./waf --run "adr --MType=Confirmed --nDownGateways=1" > "$destlog" 2>&1
+end=`date +%s`
+runtime=$((end-start))
+echo "$runtime" >> "$destlog"
+mv ./nodeData.txt ./res/nodeData21.txt
+mv ./nodeEE.txt ./res/nodeEE21.txt
+mv ./globalPerformance.txt ./res/globalPerformance21.txt
+mv ./phyPerformance.txt ./res/phyPerformance21.txt
 
-#cp ~/Github/robust-lora/res/sr_RGreedy_2_264_0.txt ./sr_loc.txt
-#cp ~/Github/robust-lora/res/gw_RGreedy_2_264_0.txt ./gw_loc.txt
-#destlog=log22
-#start=`date +%s`
-#./waf --run "adr --MType=Confirmed --nDownGateways=2" > "$destlog" 2>&1
-#end=`date +%s`
-#runtime=$((end-start))
-#echo "$runtime" >> "$destlog"
-#mv ./nodeData.txt ./res/nodeData22.txt
-#mv ./nodeEE.txt ./res/nodeEE22.txt
-#mv ./globalPerformance.txt ./res/globalPerformance22.txt
-#mv ./phyPerformance.txt ./res/phyPerformance22.txt
+cp ~/Github/robust-lora/res/sr_RGreedy_3_264_0.txt ./sr_loc.txt
+cp ~/Github/robust-lora/res/gw_RGreedy_3_264_0.txt ./gw_loc.txt
+destlog=log31
+start=`date +%s`
+./waf --run "adr --MType=Confirmed --nDownGateways=1" > "$destlog" 2>&1
+end=`date +%s`
+runtime=$((end-start))
+echo "$runtime" >> "$destlog"
+mv ./nodeData.txt ./res/nodeData31.txt
+mv ./nodeEE.txt ./res/nodeEE31.txt
+mv ./globalPerformance.txt ./res/globalPerformance31.txt
+mv ./phyPerformance.txt ./res/phyPerformance31.txt
 
-#cp ~/Github/robust-lora/res/sr_RGreedy_3_264_0.txt ./sr_loc.txt
-#cp ~/Github/robust-lora/res/gw_RGreedy_3_264_0.txt ./gw_loc.txt
-#destlog=log32
-#start=`date +%s`
-#./waf --run "adr --MType=Confirmed --nDownGateways=2" > "$destlog" 2>&1
-#end=`date +%s`
-#runtime=$((end-start))
-#echo "$runtime" >> "$destlog"
-#mv ./nodeData.txt ./res/nodeData32.txt
-#mv ./nodeEE.txt ./res/nodeEE32.txt
-#mv ./globalPerformance.txt ./res/globalPerformance32.txt
-#mv ./phyPerformance.txt ./res/phyPerformance32.txt
+cp ~/Github/robust-lora/res/sr_RGreedyc_1_264_0.txt ./sr_loc.txt
+cp ~/Github/robust-lora/res/gw_RGreedyc_1_264_0.txt ./gw_loc.txt
+destlog=log12
+start=`date +%s`
+./waf --run "adr --MType=Confirmed --nDownGateways=2" > "$destlog" 2>&1
+end=`date +%s`
+runtime=$((end-start))
+echo "$runtime" >> "$destlog"
+mv ./nodeData.txt ./res/nodeData12.txt
+mv ./nodeEE.txt ./res/nodeEE12.txt
+mv ./globalPerformance.txt ./res/globalPerformance12.txt
+mv ./phyPerformance.txt ./res/phyPerformance12.txt
+
+cp ~/Github/robust-lora/res/sr_RGreedy_2_264_0.txt ./sr_loc.txt
+cp ~/Github/robust-lora/res/gw_RGreedy_2_264_0.txt ./gw_loc.txt
+destlog=log22
+start=`date +%s`
+./waf --run "adr --MType=Confirmed --nDownGateways=2" > "$destlog" 2>&1
+end=`date +%s`
+runtime=$((end-start))
+echo "$runtime" >> "$destlog"
+mv ./nodeData.txt ./res/nodeData22.txt
+mv ./nodeEE.txt ./res/nodeEE22.txt
+mv ./globalPerformance.txt ./res/globalPerformance22.txt
+mv ./phyPerformance.txt ./res/phyPerformance22.txt
+
+cp ~/Github/robust-lora/res/sr_RGreedy_3_264_0.txt ./sr_loc.txt
+cp ~/Github/robust-lora/res/gw_RGreedy_3_264_0.txt ./gw_loc.txt
+destlog=log32
+start=`date +%s`
+./waf --run "adr --MType=Confirmed --nDownGateways=2" > "$destlog" 2>&1
+end=`date +%s`
+runtime=$((end-start))
+echo "$runtime" >> "$destlog"
+mv ./nodeData.txt ./res/nodeData32.txt
+mv ./nodeEE.txt ./res/nodeEE32.txt
+mv ./globalPerformance.txt ./res/globalPerformance32.txt
+mv ./phyPerformance.txt ./res/phyPerformance32.txt
 
 cp ~/Github/robust-lora/res/sr_ICIOT_264_0.txt ./sr_loc.txt
 cp ~/Github/robust-lora/res/gw_ICIOT_264_0.txt ./gw_loc.txt
