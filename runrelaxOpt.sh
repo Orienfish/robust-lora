2e6d7f2bf9f28b11785239eca5d0fd0bcac0ea81
Orienfish <x1yu@ucsd.edu>
2020-10-25 20:20:34 -0700

    add exit

--- runrelaxOpt.sh
+++ runrelaxOpt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for sr_cnt in 75 100 125
+for sr_cnt in 1 2 3 4 5
 do
 	echo "$sr_cnt"
-	python3 main.py --sr_cnt $sr_cnt
-	/Applications/MATLAB_R2020b.app/bin/matlab -nodisplay -nosplash - nodesktop -r "run /Users/xiaofanyu/Github/robust-lora/relaxOpt/relaxedOpt.m"
+	python3 main.py # --sr_cnt $sr_cnt
+	/Applications/MATLAB_R2020b.app/bin/matlab -nodisplay -nosplash - nodesktop -r "run /Users/xiaofanyu/Github/robust-lora/relaxOpt/relaxedOpt.m; exit;"
 done
