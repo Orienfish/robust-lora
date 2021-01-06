4837f918be6d9d45c4757f87b0afeb35ee278bde
Orienfish <x1yu@ucsd.edu>
2021-01-06 15:01:34 -0800

    update gw exp ns3

--- ns3-exp/run-gw.sh
+++ ns3-exp/run-gw.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-cp ~/Github/robust-lora/res/sr_RGreedyc_1_264_0.txt ./sr_loc.txt
-cp ~/Github/robust-lora/res/gw_RGreedyc_1_264_0.txt ./gw_loc.txt
+cp ~/Github/robust-lora/res/sr_RGreedy_1_264_0dp.txt ./sr_loc.txt
+cp ~/Github/robust-lora/res/gw_RGreedy_1_264_0dp.txt ./gw_loc.txt
+cp ~/Github/robust-lora/res/pl_RGreedy_1_264_0dp.txt ./pl_mat.txt
 destlog=log1
 start=`date +%s`
-./waf --run "adr --MType=Confirmed" > "$destlog" 2>&1
+./waf --run "adr --MType=Confirmed --MatrixPLEnabled" > "$destlog" 2>&1
 end=`date +%s`
 runtime=$((end-start))
 echo "$runtime" >> "$destlog"
@@ -13,11 +14,12 @@ mv ./nodeEE.txt ./res/nodeEE1.txt
 mv ./globalPerformance.txt ./res/globalPerformance1.txt
 mv ./phyPerformance.txt ./res/phyPerformance1.txt
 
-cp ~/Github/robust-lora/res/sr_RGreedy_2_264_0.txt ./sr_loc.txt
-cp ~/Github/robust-lora/res/gw_RGreedy_2_264_0.txt ./gw_loc.txt
+cp ~/Github/robust-lora/res/sr_RGreedy_2_264_0dp.txt ./sr_loc.txt
+cp ~/Github/robust-lora/res/gw_RGreedy_2_264_0dp.txt ./gw_loc.txt
+cp ~/Github/robust-lora/res/pl_RGreedy_2_264_0dp.txt ./pl_mat.txt
 destlog=log2
 start=`date +%s`
-./waf --run "adr --MType=Confirmed" > "$destlog" 2>&1
+./waf --run "adr --MType=Confirmed --MatrixPLEnabled" > "$destlog" 2>&1
 end=`date +%s`
 runtime=$((end-start))
 echo "$runtime" >> "$destlog"
@@ -26,11 +28,12 @@ mv ./nodeEE.txt ./res/nodeEE2.txt
 mv ./globalPerformance.txt ./res/globalPerformance2.txt
 mv ./phyPerformance.txt ./res/phyPerformance2.txt
 
-cp ~/Github/robust-lora/res/sr_RGreedy_3_264_0.txt ./sr_loc.txt
-cp ~/Github/robust-lora/res/gw_RGreedy_3_264_0.txt ./gw_loc.txt
+cp ~/Github/robust-lora/res/sr_RGreedy_3_264_0dp.txt ./sr_loc.txt
+cp ~/Github/robust-lora/res/gw_RGreedy_3_264_0dp.txt ./gw_loc.txt
+cp ~/Github/robust-lora/res/pl_RGreedy_3_264_0dp.txt ./pl_mat.txt
 destlog=log3
 start=`date +%s`
-./waf --run "adr --MType=Confirmed" > "$destlog" 2>&1
+./waf --run "adr --MType=Confirmed --MatrixPLEnabled" > "$destlog" 2>&1
 end=`date +%s`
 runtime=$((end-start))
 echo "$runtime" >> "$destlog"
@@ -39,11 +42,12 @@ mv ./nodeEE.txt ./res/nodeEE3.txt
 mv ./globalPerformance.txt ./res/globalPerformance3.txt
 mv ./phyPerformance.txt ./res/phyPerformance3.txt
 
-cp ~/Github/robust-lora/res/sr_RGreedyc_1_264_0.txt ./sr_loc.txt
-cp ~/Github/robust-lora/res/gw_RGreedyc_1_264_0.txt ./gw_loc.txt
+cp ~/Github/robust-lora/res/sr_RGreedy_1_264_0dp.txt ./sr_loc.txt
+cp ~/Github/robust-lora/res/gw_RGreedy_1_264_0dp.txt ./gw_loc.txt
+cp ~/Github/robust-lora/res/pl_RGreedy_1_264_0dp.txt ./pl_mat.txt
 destlog=log11
 start=`date +%s`
-./waf --run "adr --MType=Confirmed --nDownGateways=1" > "$destlog" 2>&1
+./waf --run "adr --MType=Confirmed --nDownGateways=1 --MatrixPLEnabled" > "$destlog" 2>&1
 end=`date +%s`
 runtime=$((end-start))
 echo "$runtime" >> "$destlog"
@@ -52,11 +56,12 @@ mv ./nodeEE.txt ./res/nodeEE11.txt
 mv ./globalPerformance.txt ./res/globalPerformance11.txt
 mv ./phyPerformance.txt ./res/phyPerformance11.txt
 
-cp ~/Github/robust-lora/res/sr_RGreedy_2_264_0.txt ./sr_loc.txt
-cp ~/Github/robust-lora/res/gw_RGreedy_2_264_0.txt ./gw_loc.txt
+cp ~/Github/robust-lora/res/sr_RGreedy_2_264_0dp.txt ./sr_loc.txt
+cp ~/Github/robust-lora/res/gw_RGreedy_2_264_0dp.txt ./gw_loc.txt
+cp ~/Github/robust-lora/res/pl_RGreedy_2_264_0dp.txt ./pl_mat.txt
 destlog=log21
 start=`date +%s`
-./waf --run "adr --MType=Confirmed --nDownGateways=1" > "$destlog" 2>&1
+./waf --run "adr --MType=Confirmed --nDownGateways=1 --MatrixPLEnabled" > "$destlog" 2>&1
 end=`date +%s`
 runtime=$((end-start))
 echo "$runtime" >> "$destlog"
@@ -65,11 +70,12 @@ mv ./nodeEE.txt ./res/nodeEE21.txt
 mv ./globalPerformance.txt ./res/globalPerformance21.txt
 mv ./phyPerformance.txt ./res/phyPerformance21.txt
 
-cp ~/Github/robust-lora/res/sr_RGreedy_3_264_0.txt ./sr_loc.txt
-cp ~/Github/robust-lora/res/gw_RGreedy_3_264_0.txt ./gw_loc.txt
+cp ~/Github/robust-lora/res/sr_RGreedy_3_264_0dp.txt ./sr_loc.txt
+cp ~/Github/robust-lora/res/gw_RGreedy_3_264_0dp.txt ./gw_loc.txt
+cp ~/Github/robust-lora/res/pl_RGreedy_3_264_0dp.txt ./pl_mat.txt
 destlog=log31
 start=`date +%s`
-./waf --run "adr --MType=Confirmed --nDownGateways=1" > "$destlog" 2>&1
+./waf --run "adr --MType=Confirmed --nDownGateways=1 --MatrixPLEnabled" > "$destlog" 2>&1
 end=`date +%s`
 runtime=$((end-start))
 echo "$runtime" >> "$destlog"
@@ -78,11 +84,12 @@ mv ./nodeEE.txt ./res/nodeEE31.txt
 mv ./globalPerformance.txt ./res/globalPerformance31.txt
 mv ./phyPerformance.txt ./res/phyPerformance31.txt
 
-cp ~/Github/robust-lora/res/sr_RGreedyc_1_264_0.txt ./sr_loc.txt
-cp ~/Github/robust-lora/res/gw_RGreedyc_1_264_0.txt ./gw_loc.txt
+cp ~/Github/robust-lora/res/sr_RGreedy_1_264_0dp.txt ./sr_loc.txt
+cp ~/Github/robust-lora/res/gw_RGreedy_1_264_0dp.txt ./gw_loc.txt
+cp ~/Github/robust-lora/res/pl_RGreedy_1_264_0dp.txt ./pl_mat.txt
 destlog=log12
 start=`date +%s`
-./waf --run "adr --MType=Confirmed --nDownGateways=2" > "$destlog" 2>&1
+./waf --run "adr --MType=Confirmed --nDownGateways=2 --MatrixPLEnabled" > "$destlog" 2>&1
 end=`date +%s`
 runtime=$((end-start))
 echo "$runtime" >> "$destlog"
@@ -91,11 +98,12 @@ mv ./nodeEE.txt ./res/nodeEE12.txt
 mv ./globalPerformance.txt ./res/globalPerformance12.txt
 mv ./phyPerformance.txt ./res/phyPerformance12.txt
 
-cp ~/Github/robust-lora/res/sr_RGreedy_2_264_0.txt ./sr_loc.txt
-cp ~/Github/robust-lora/res/gw_RGreedy_2_264_0.txt ./gw_loc.txt
+cp ~/Github/robust-lora/res/sr_RGreedy_2_264_0dp.txt ./sr_loc.txt
+cp ~/Github/robust-lora/res/gw_RGreedy_2_264_0dp.txt ./gw_loc.txt
+cp ~/Github/robust-lora/res/pl_RGreedy_2_264_0dp.txt ./pl_mat.txt
 destlog=log22
 start=`date +%s`
-./waf --run "adr --MType=Confirmed --nDownGateways=2" > "$destlog" 2>&1
+./waf --run "adr --MType=Confirmed --nDownGateways=2 --MatrixPLEnabled" > "$destlog" 2>&1
 end=`date +%s`
 runtime=$((end-start))
 echo "$runtime" >> "$destlog"
@@ -104,11 +112,12 @@ mv ./nodeEE.txt ./res/nodeEE22.txt
 mv ./globalPerformance.txt ./res/globalPerformance22.txt
 mv ./phyPerformance.txt ./res/phyPerformance22.txt
 
-cp ~/Github/robust-lora/res/sr_RGreedy_3_264_0.txt ./sr_loc.txt
-cp ~/Github/robust-lora/res/gw_RGreedy_3_264_0.txt ./gw_loc.txt
+cp ~/Github/robust-lora/res/sr_RGreedy_3_264_0dp.txt ./sr_loc.txt
+cp ~/Github/robust-lora/res/gw_RGreedy_3_264_0dp.txt ./gw_loc.txt
+cp ~/Github/robust-lora/res/pl_RGreedy_3_264_0dp.txt ./pl_mat.txt
 destlog=log32
 start=`date +%s`
-./waf --run "adr --MType=Confirmed --nDownGateways=2" > "$destlog" 2>&1
+./waf --run "adr --MType=Confirmed --nDownGateways=2 --MatrixPLEnabled" > "$destlog" 2>&1
 end=`date +%s`
 runtime=$((end-start))
 echo "$runtime" >> "$destlog"
@@ -117,11 +126,12 @@ mv ./nodeEE.txt ./res/nodeEE32.txt
 mv ./globalPerformance.txt ./res/globalPerformance32.txt
 mv ./phyPerformance.txt ./res/phyPerformance32.txt
 
-cp ~/Github/robust-lora/res/sr_ICIOT_264_0.txt ./sr_loc.txt
-cp ~/Github/robust-lora/res/gw_ICIOT_264_0.txt ./gw_loc.txt
+cp ~/Github/robust-lora/res/sr_ICIOT_6_264_0dp.txt ./sr_loc.txt
+cp ~/Github/robust-lora/res/gw_ICIOT_6_264_0dp.txt ./gw_loc.txt
+cp ~/Github/robust-lora/res/pl_ICIOT_6_264_0dp.txt ./pl_mat.txt
 destlog=logiciot
 start=`date +%s`
-./waf --run "adr --MType=Confirmed" > "$destlog" 2>&1
+./waf --run "adr --MType=Confirmed --MatrixPLEnabled" > "$destlog" 2>&1
 end=`date +%s`
 runtime=$((end-start))
 echo "$runtime" >> "$destlog"
@@ -130,11 +140,12 @@ mv ./nodeEE.txt ./res/nodeEEiciot.txt
 mv ./globalPerformance.txt ./res/globalPerformanceiciot.txt
 mv ./phyPerformance.txt ./res/phyPerformanceiciot.txt
 
-cp ~/Github/robust-lora/res/sr_ICIOT_264_0.txt ./sr_loc.txt
-cp ~/Github/robust-lora/res/gw_ICIOT_264_0.txt ./gw_loc.txt
+cp ~/Github/robust-lora/res/sr_ICIOT_9_264_0dp.txt ./sr_loc.txt
+cp ~/Github/robust-lora/res/gw_ICIOT_9_264_0dp.txt ./gw_loc.txt
+cp ~/Github/robust-lora/res/pl_ICIOT_9_264_0dp.txt ./pl_mat.txt
 destlog=logiciot1
 start=`date +%s`
-./waf --run "adr --MType=Confirmed --nDownGateways=1" > "$destlog" 2>&1
+./waf --run "adr --MType=Confirmed --nDownGateways=1 --MatrixPLEnabled" > "$destlog" 2>&1
 end=`date +%s`
 runtime=$((end-start))
 echo "$runtime" >> "$destlog"
@@ -143,11 +154,12 @@ mv ./nodeEE.txt ./res/nodeEEiciot1.txt
 mv ./globalPerformance.txt ./res/globalPerformanceiciot1.txt
 mv ./phyPerformance.txt ./res/phyPerformanceiciot1.txt
 
-cp ~/Github/robust-lora/res/sr_ICIOT_264_0.txt ./sr_loc.txt
-cp ~/Github/robust-lora/res/gw_ICIOT_264_0.txt ./gw_loc.txt
+cp ~/Github/robust-lora/res/sr_ICIOT_12_264_0dp.txt ./sr_loc.txt
+cp ~/Github/robust-lora/res/gw_ICIOT_12_264_0dp.txt ./gw_loc.txt
+cp ~/Github/robust-lora/res/pl_ICIOT_12_264_0dp.txt ./pl_mat.txt
 destlog=logiciot2
 start=`date +%s`
-./waf --run "adr --MType=Confirmed --nDownGateways=2" > "$destlog" 2>&1
+./waf --run "adr --MType=Confirmed --nDownGateways=2 --MatrixPLEnabled" > "$destlog" 2>&1
 end=`date +%s`
 runtime=$((end-start))
 echo "$runtime" >> "$destlog"
