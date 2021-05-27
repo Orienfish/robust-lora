9dcca1fdeea20e1110255fc9cbb348500d889a03
Orienfish <x1yu@ucsd.edu>
2021-05-26 20:57:22 -0700

    fix bug

--- opt/pdr.m
+++ opt/pdr.m
@@ -70,16 +70,13 @@ end
 gw_extract = [eye(params.gw_cnt), zeros(params.gw_cnt, params.var_cnt - params.gw_cnt)];
 g = gw_extract * x;
 
-% Factorize the form of PDR constraints, let c <= 0
-c = log(1 - P_ij) * g - log(1 - params.PDR_th);
-
 % Compute PDR at i
-%PDR_i = zeros(params.sr_cnt, 1);
-%for i = 1:params.sr_cnt
-%    P = 1 - P_ij(i, :)' .* g;
-%    PDR_i(i) = 1 - prod(P);
-%end
-%PDR_i'
+PDR_i = zeros(params.sr_cnt, 1);
+for i = 1:params.sr_cnt
+    P = 1 - P_ij(i, :)' .* g;
+    PDR_i(i) = 1 - prod(P);
+end
+PDR_i'
 
 % Add nonlinear lifetime constraint
 %Pow_i = zeros(params.sr_cnt, 1);
@@ -98,7 +95,6 @@ c = log(1 - P_ij) * g - log(1 - params.PDR_th);
 %Pow_i = Pow_i / params.Time;
 %Pow_i'
 
-%c = [params.PDR_th - PDR_i; Pow_i - params.E_batt/params.L_th];
-%c = params.PDR_th - PDR_i;
+c = params.PDR_th - PDR_i;
 ceq = [];
 end
